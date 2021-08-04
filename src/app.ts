6b6853403a440a7f2bfbd7252740d9e6a24c1c4c
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-05 02:47:50 +0600

    preview static site added

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,18 @@ import dbConnect from './db/connect';
 import logger from './logger';
 import errorHandler from './middlewares/errorHandler';
 
+import staticRoutes from './static/static.route';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
+
+// Static Site Start
+app.engine('.html', require('ejs').__express);
+app.use('/static', express.static(path.join(__dirname, 'static/public')));
+app.set('views', path.join(__dirname, 'static/views'));
+app.set('view engine', 'html');
+// Static Site End
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
@@ -17,6 +26,9 @@ app.listen(APP_PORT || 8000, () => {
   
   // Database
   dbConnect();
+  
+  // Static site route
+  app.use(staticRoutes)
   
   // Routes
   routes(app);
