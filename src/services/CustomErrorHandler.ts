fcd78ca7b101ba6c08e881d6238a6032d2c4b5b1
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-04 22:32:18 +0600

    instance of error fixed

--- src/services/CustomErrorHandler.ts
+++ src/services/CustomErrorHandler.ts
@@ -1,22 +1,26 @@
 class CustomErrorHandler extends Error {
   status: number;
-  constructor(status: number, msg: string) {
+  constructor(status: number, message: string) {
     super();
     this.status = status;
-    this.message = msg;
+    this.message = message;
+    Object.setPrototypeOf(this, CustomErrorHandler.prototype);
   }
   static alreadyExists(message: string) {
     return new CustomErrorHandler(409, message)
   }
-  static invalidCredentials(message: string='User or Password is Wrong.') {
+  static invalidCredentials(message='User or Password is Wrong.') {
     return new CustomErrorHandler(401, message)
   }
-  static unAuthorization(message: string="Access Denied") {
+  static unAuthorization(message="Access Denied") {
     return new CustomErrorHandler(401, message)
   }
-  static serverError(message: string="Internal Server Error") {
+  static serverError(message="Internal Server Error") {
     return new CustomErrorHandler(500, message)
+  }
+  static notAllow(message="Not allow") {
+    return new CustomErrorHandler(405, message)
   }
 }
 
-export default CustomErrorHandler
+export default CustomErrorHandler;
