7af2c7e3a9e1e70ed9cdecca836e4ec775a925f8
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-08 01:34:46 +0600

    category implemented

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,97 @@
+import { Request, Response, NextFunction } from "express";
+import slugify from "slugify";
+import { Category } from "../models";
+import CustomErrorHandler from "../services/CustomErrorHandler";
+import categorySchema from '../validates/category.validate';
+
+const categoryController = {
+  categories: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const _categories = await Category.find();
+      return res.json({ data: _categories, status: 200, message: "Success" });
+    } catch (err) {
+      return next(CustomErrorHandler.serverError())
+    }
+  },
+  create: async (req: Request, res: Response, next: NextFunction) => {
+    const { error } = categorySchema.validate(req.body);
+    if (error) {
+      return next(error)
+    }
+    const obj = new Category({
+      name: req.body.name,
+      parent: req.body.parent
+    });
+    try {
+      if(req.isSuperAdmin) {
+        const category = await obj.save();
+        return res.json({data: category, status: 201, message: 'Success! Category created by admin'})
+      }
+      const category = {
+        _id: obj._id,
+        name: obj.name,
+        slug: slugify(obj.name, { lower: true }),
+        parent: obj.parent
+      }
+      return res.json({data: category, status: 201, message: 'Success! Category created'})
+    } catch(err) {
+      return next(CustomErrorHandler.serverError())
+    }
+  },
+  single: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const category = await Category.findOne({slug: req.params.slug}).populate('products');
+      if(category) {
+        return next(CustomErrorHandler.notFound())
+      }
+      return res.json({ data: category, status: 200, message: "Success" });
+    } catch (err) {
+      return next(CustomErrorHandler.serverError())
+    }
+  },
+  update: async (req: Request, res: Response, next: NextFunction) => {
+    const { error } = categorySchema.validate(req.body);
+    if (error) {
+      return next(error)
+    }
+    const obj = new Category({
+      name: req.body.name,
+      parent: req.body.parent
+    });
+    try {
+      if(req.isSuperAdmin) {
+        const category = await obj.save();
+        return res.json({data: category, status: 201, message: 'Success! Category updated by admin'})
+      }
+      const category = {
+        _id: obj._id,
+        name: obj.name,
+        slug: slugify(obj.name, { lower: true }),
+        parent: obj.parent
+      }
+      return res.json({data: category, status: 201, message: 'Success! Category updated'})
+    } catch(err) {
+      return next(CustomErrorHandler.serverError())
+    }
+  },
+  delete: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if(req.isSuperAdmin) {
+        const instance = await Category.findOneAndDelete({slug: req.params.slug})
+        if (!instance) {
+          return next(CustomErrorHandler.notFound('Category is not found!'))
+        }
+        return res.json({status: 202, message: 'Success! Category deleted by admin'})
+      }
+      const instance = await Category.find({slug: req.params.slug});
+      if (!instance) {
+        return next(CustomErrorHandler.notFound('Category is not found!'))
+      }
+      return res.json({status: 202, message: 'Success! Category deleted'})
+     } catch (err) {
+       return next(CustomErrorHandler.notFound('Category is not found!'))
+     }
+  },
+}
+
+export default categoryController;
--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,24 +1,27 @@
-import { Schema, Document, model, HookNextFunction } from 'mongoose';
+import { Schema, Document, model, HookNextFunction, PopulatedDoc } from 'mongoose';
 import slugify from 'slugify';
 import Utils from '../services/Utils';
+import { ProductDocument } from './product.model';
 
 export interface CategoryDocument extends Document {
   name: string;
   slug: string;
-  parentId?: string;
+  parent?: PopulatedDoc<'self' & Document>;
+  // products: PopulatedDoc<ProductDocument & Document>[];
 }
 
 
 const CategorySchema = new Schema<CategoryDocument>({
   name: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
-  parentId: { type: String, required: false },
+  slug: { type: String, required: false, unique: true },
+  parent: { type: 'ObjectId', ref: 'Category' },
+  // products: [{ type: 'ObjectId', ref: 'Product' }],
 })
 
 CategorySchema.pre('save', async function(next: HookNextFunction) {
   let obj = this as CategoryDocument;
-  if (!obj.slug || obj.isModified('slug')) {
-    let newSlug = slugify(obj.name)
+  if (!obj.slug || obj.isModified('name')) {
+    let newSlug = slugify(obj.name, { lower: true })
     const isExist = await Category.exists({slug: newSlug});
     if (isExist) {
       newSlug = newSlug + Utils.getRandomString();
@@ -27,6 +30,6 @@ CategorySchema.pre('save', async function(next: HookNextFunction) {
   }
   return next()
 })
-const Category = model('Category', CategorySchema, 'categories');
+const Category = model<CategoryDocument>('Category', CategorySchema, 'categories');
 
 export default Category;
