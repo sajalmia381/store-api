10c5f45b020bfe0ba916b9cf4499dbf57a7306cf
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-07 14:16:41 +0600

    some style changed

--- src/static/public/js/script.js
+++ src/static/public/js/script.js
@@ -1,9 +1,22 @@
 (function ($) {
   "use strict";
-  console.log('script work');
+  	/* smooth Scroll */
+	$('a[href*="#"]:not([href="#').on('click', function () {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				HtmlBody.animate({
+					scrollTop: target.offset().top - 70
+				}, 1000);
+				return false;
+			}
+		}
+	});
+  
+  // Home try
   var siteBaseUrl = window.location.origin;
   $('#response_try_btn').on('click', function(e) {
-    
     const endpoint = siteBaseUrl + '/api/products/running-shoes';
     const tryLoaderEle = $("#try__loader");
     const preEle = $('#response_try_pre');
