f2895cb9270afde25517e9dfbadae5f7ea5cba50
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-08 01:34:46 +0600

    category implemented

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,5 +1,17 @@
 import { Router } from 'express';
+import { categoryController } from '../controllers';
+import attachUser from '../middlewares/attachUser';
 
 const router = Router();
+
+router.get('/categories', categoryController.categories);
+
+router.post('/category', attachUser, categoryController.create);
+
+router.get('/category/:slug', categoryController.single);
+
+router.put('/category/:slug', attachUser, categoryController.update);
+
+router.delete('/category/:slug', attachUser, categoryController.delete);
 
 export default router;
