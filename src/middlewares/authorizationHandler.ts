73e0949b0210ca90ab81b9370bca54fe2817cbbb
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-07 10:16:22 +0600

    minus change

--- src/middlewares/authorizationHandler.ts
+++ src/middlewares/authorizationHandler.ts
@@ -3,12 +3,6 @@ import { JwtPayload } from "jsonwebtoken";
 import CustomErrorHandler from "../services/CustomErrorHandler";
 import JwtService from "../services/JwtService";
 
-interface User extends JwtPayload {
-  _id: string;
-  email: string;
-  role: string;
-}
-
 const authorizationHandler = (req: Request, res: Response, next: NextFunction) => {
   const headerAuthorization = req.headers.authorization;
   if (!headerAuthorization) {
@@ -16,8 +10,6 @@ const authorizationHandler = (req: Request, res: Response, next: NextFunction) =
   }
   const token = headerAuthorization.split(' ')[1];
   try {
-    const payload = JwtService.verify(token);
-    // console.log(payload) 
     const { _id, email, role } = JwtService.verify(token) as JwtPayload;
     const user = { _id, email, role }
     req.user = user;
