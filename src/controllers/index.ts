7ae402f3c805bbe0457dfed3de39a3b4e87271b2
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-08 01:35:18 +0600

    added category and user

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,4 @@
 export { default as authController } from './auth.controller';
 export { default as productController } from './product.controller';
+export { default as userController } from './user.controller';
+export { default as categoryController } from './category.controller';
