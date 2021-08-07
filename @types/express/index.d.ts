90f3a60b37c50c079581b747fbccd0bbac012af1
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-07 10:17:11 +0600

    req: req super user type added

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -3,7 +3,8 @@ import User from "../../src/models";
 declare global{
     namespace Express {
         interface Request {
-            user: User
+            user?: User,
+            isSuperAdmin?: boolean,
         }
     }
 }
