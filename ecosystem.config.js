7a65901eb3b92a010888977c3554737502407931
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-29 20:32:39 +0600

    prod fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
         APP_PORT: 8000,
         APP_HOST: "localhost",
         DB_URL: "mongodb://localhost:27017/store-api",
-        DEBUG_MODE="false",
         NODE_ENV="production",
         SECRET_KEY="!rad9TN`&B{!pg&Y($oTwOhYfEJ4{0sEBYtu]xqa2^8,Iyc^8HP[[XB,J$O)~BL",
         REFRESH_KEY="hOS1|4NM.P#F^BA]JiIX^B0AS@JY+E20Zphjs2ER[}FMJXq3}1vH~iGrQPqtycv",
