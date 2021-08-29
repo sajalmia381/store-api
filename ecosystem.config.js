0963dba32326ffbfe45138a3fbdbd5ac3c954f34
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-29 21:04:46 +0600

    prod: pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,9 @@ module.exports = {
     {
       script: 'src/app.ts',
       watch: '.',
+      interpreter: "./node_modules/.bin/ts-node",
+      interpreter_args: "--require ts-node/register --require tsconfig-paths/register",
+      merge_logs: true,
       "env": {
         APP_PORT: 8000,
         APP_HOST: "localhost",
@@ -12,10 +15,7 @@ module.exports = {
         SECRET_KEY: "ipfhUCFRZ4fce7GWaatUZMQbQ655sygRq0KUATOUX4VB1RFjaUtl6lfKmlP4AHg8",
         REFRESH_KEY: "ipfhUCFRZ4fce7GWaatUZMQbQ655sygRq0KUATOUX4VB1RFjaUtl6lfKmlP4AHg8",
       }
-    },
-    {
-      "extends": "../tsconfig.json"
-    } 
+    }
     // {
     //   script: './service-worker/',
     //   watch: ['./service-worker']
