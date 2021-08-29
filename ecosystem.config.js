3b03237b434a3d5ba74d3a680731121f50f56d9b
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-30 00:13:17 +0600

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   apps : [
     {
+      name: 'store-api',
       script: 'src/app.ts',
       watch: '.',
-      interpreter: "./node_modules/.bin/ts-node",
-      interpreter_args: "--require tsconfig-paths/register",
+      // interpreter: "./node_modules/.bin/ts-node",
+      // interpreter_args: "--require tsconfig-paths/register",
       merge_logs: true,
       "env": {
         APP_PORT: 8000,
@@ -16,22 +17,5 @@ module.exports = {
         REFRESH_KEY: "hOS1|4NM.P#F^BA]JiIX^B0AS@JY+E20Zphjs2ER[}FMJXq3}1vH~iGrQPqtycv",
       }
     }
-    // {
-    //   script: './service-worker/',
-    //   watch: ['./service-worker']
-    // }
   ],
-
-  // deploy : {
-  //   production : {
-  //     user : 'SSH_USERNAME',
-  //     host : 'SSH_HOSTMACHINE',
-  //     ref  : 'origin/master',
-  //     repo : 'GIT_REPOSITORY',
-  //     path : 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
 };
