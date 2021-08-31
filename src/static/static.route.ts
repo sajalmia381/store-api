75cfe378be6f0f4c35e8cb306e68532f4b1d5764
Md Sajal Mia <sajalmia381@gmail.com>
2021-08-31 22:47:24 +0600

    minor

--- src/static/static.route.ts
+++ src/static/static.route.ts
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
   const path_name: string = res.req.originalUrl;
-  console.log(req.protocol, req.hostname, req.baseUrl)
   res.render('index', { path_name })
 })
 
