dda7a0d5e32c20ece54c0cabb2650a494652c359
Md Sajal Mia <sajalmia381@gmail.com>
2021-09-02 09:22:29 +0600

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,8 @@ module.exports = {
       max_memory_restart: "256M",
       exec_mode: "cluster",
       interpreter: './node_modules/.bin/ts-node',
-      interpreter_args: '-r tsconfig-paths/register',
-      node_args: '-r tsconfig-paths/register',
+      // interpreter_args: '-r tsconfig-paths/register',
+      // node_args: '-r tsconfig-paths/register',
       "env": {
         APP_PORT: 8000,
         APP_HOST: "localhost",
