f2a7de7bb63fb7b0110c7b5db9b8b7e3adc71ddb
Md Sajal Mia <sajalmia381@gmail.com>
2021-11-20 16:43:00 +0600

    api prefix remove for production

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,21 +7,19 @@ import categoryRoutes from './category.route';
 import imageRoute from './image.route';
 import cartRoute from './cart.route';
 
-const API_PREFIX = '/api'
-
 export default function (app: Express) {
   app.get('/health-check', (req: Request, res: Response) => res.sendStatus(200));
   
   // Auth routes
-  app.use(API_PREFIX + '/auth', authRoutes);
+  app.use('/auth', authRoutes);
   // User routes
-  app.use(API_PREFIX + '/users', userRoutes);
+  app.use('/users', userRoutes);
   // Product Routes
-  app.use(API_PREFIX + '/products', productRoutes)
+  app.use('/products', productRoutes)
   // Image
-  app.use(API_PREFIX + '/images', imageRoute)
+  app.use('/images', imageRoute)
   // Cart
-  app.use(API_PREFIX + '/carts', cartRoute)
+  app.use('/carts', cartRoute)
   // Category
-  app.use(API_PREFIX + '', categoryRoutes)
+  app.use('', categoryRoutes)
 }
