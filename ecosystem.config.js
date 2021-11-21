a7a57ff2581045048e2ba648528726c74e4cbefc
Md Sajal Mia <sajalmia381@gmail.com>
2021-11-21 16:46:21 +0600

    minor fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       max_memory_restart: "256M",
       exec_mode: "cluster",
       interpreter: './node_modules/.bin/ts-node',
-      interpreter_args: '-r ts-node/register -r tsconfig-paths/register',
+      interpreter_args: '--require ts-node/register --require tsconfig-paths/register',
       // node_args: '-r tsconfig-paths/register',
       "env": {
         APP_PORT: 8000,
