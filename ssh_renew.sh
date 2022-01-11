57b0c6e3d64bc05a1ae23c95462a23ae1027629c
Md Sajal Mia <sajalmia381@gmail.com>
2022-01-11 21:22:57 +0600

    minor change

--- ssh_renew.sh
+++ ssh_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/sammy/node_project/
+cd /home/ubuntu/store-api/
 $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
