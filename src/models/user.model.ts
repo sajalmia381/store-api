4b88b998f4b46d378bf62c14654a3f0a62b23b65
Md Sajal Mia <sajalmia381@gmail.com>
2022-01-22 13:25:56 +0600

    arm machine build fix

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, model, Document } from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export interface UserDocument extends Document {
   name: string;
