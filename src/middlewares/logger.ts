b6d99f22eac8ace45f192d0be00ab5acd6765fc9
Md Sajal Mia <sajalmia381@gmail.com>
2022-09-07 00:59:28 +0600

    logger improvement

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import logger from "../logger";
 
 const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  logger.info(`method=${req.method}, uri=${req.url}, status=${res.statusCode}`) // , latency=6.238609ms
+  logger.info(`method=${req.method}, uri=${req.originalUrl}, status=${res.statusCode} ip=${req.headers?.['x-forwarded-for'] || req.socket.remoteAddress}`) // , latency=6.238609ms
   return next()
 }
 
