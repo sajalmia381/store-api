fcf1cee43ce8c8219f5e24d1c8f5155d4d1ce366
Md. Sajal Mia <sajalmia381@gmail.com>
2023-11-12 00:00:05 +0600

    implement todo list create controller

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -4,14 +4,14 @@ import attachUser from '../middlewares/attachUser';
 
 const router = Router();
 
-router.get('/categories', categoryController.categories);
+router.get('/', categoryController.categories);
 
-router.post('/categories', attachUser, categoryController.create);
+router.post('/', attachUser, categoryController.create);
 
-router.get('/categories/:slug', categoryController.single);
+router.get('/:slug', categoryController.single);
 
-router.put('/categories/:slug', attachUser, categoryController.update);
+router.put('/:slug', attachUser, categoryController.update);
 
-router.delete('/categories/:slug', attachUser, categoryController.delete);
+router.delete('/:slug', attachUser, categoryController.delete);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import productRoutes from './product.route';
 import categoryRoutes from './category.route';
 import imageRoute from './image.route';
 import cartRoute from './cart.route';
+import todoRoute from './todo.route';
 
 export default function (app: Express) {
   app.get('/health-check', (req: Request, res: Response) => res.sendStatus(200));
@@ -21,5 +22,7 @@ export default function (app: Express) {
   // Cart
   app.use('/carts', cartRoute)
   // Category
-  app.use('', categoryRoutes)
+  app.use('categories', categoryRoutes)
+  // Todo
+  app.use('/todos', todoRoute)
 }
