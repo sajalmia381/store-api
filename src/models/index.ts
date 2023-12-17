483a365c82f780dffd0ee6fa172084848de91ed0
Md. Sajal Mia <sajalmia381@gmail.com>
2023-12-18 02:34:08 +0600

    feat(todo): CURD implemente

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,4 +4,4 @@ export { default as Product } from './product.model';
 export { default as Category } from './category.model';
 export { default as Image } from './image.model';
 export { default as Cart } from './cart.model';
-export { default as Todo } from './todo.model';
+export { default as Todo, TodoStatus, isCompleteStatus } from './todo.model';
--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { todoController } from "../controllers";
+import attachUser from "../middlewares/attachUser";
 
 const router = Router();
 
 router.get("/", todoController.list);
-router.post("/", todoController.create);
+router.post("/", attachUser, todoController.create);
+router.put("/:id", attachUser, todoController.update);
+router.delete("/:id", attachUser, todoController.destroy);
 
 export default router;
